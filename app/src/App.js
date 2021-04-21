2063be49b7a512d7d85f2bf192ba91e608fa089b
bbrad3 <68521389+bbrad3@users.noreply.github.com>
2021-04-21 15:54:52 -0600

    Btns input to screen, computeAnswer does not work yet

--- app/src/App.js
+++ app/src/App.js
@@ -23,18 +23,57 @@ const btnValues = [
   {id: 16, char: 'enter'}
 ]
 
+// Brackets => pOwers => DivMult => AddSub
+const computeAnswer = (str) => { // NO WORKY!!!!!!
+  console.log('str', str);
+  let split1 = str.split('+')
+  console.log('split1', split1);
+
+  let split2 = []
+  split1.forEach(el => split2.push(el.split('-')))
+  console.log('split2', split2);
+
+  let split3 = []
+  split2.forEach(el => {
+    el.forEach(elem => split3.push(elem.split('*')))
+  })
+  console.log('split3', split3);
+
+  let split4 = []
+  split3.forEach(el => {
+    el.forEach(elem => split4.push(elem.split('/')))
+  })
+  console.log('split4', split4);
+
+  let multiply = 0
+  let divide = 0
+  let add = 0
+  let subtract = 0
+  // split4.forEach(arr => {
+  //   arr.forEach
+  // })
+
+  multiply = split4[1][0] * split4[2][0]
+  divide = split4[3][0] / split4[3][1]
+  add = split4[0][0] + multiply
+  subtract = multiply - divide
+  let answer = subtract
+
+  console.log('answer', answer);
+  // setAnswer(answer)
+}
+
 function App() {
   const [input, setInput] = useState('')
+  const [answer, setAnswer] = useState('')
 
   return (
     <div className="App">
       <div className="calcBody">
 
         <div className="calcHeader">
-          <div className="calcScreen">
-            <input type="text"  />
-          </div>
-          <div className="calcAnswer"></div>
+          <div className="calcScreen">{input}</div>
+          <div className="calcAnswer">{answer}</div>
         </div>
 
         <div className="calcModal">
@@ -49,6 +88,9 @@ function App() {
                 key = {character.id}
                 id = {character.id}
                 char = {character.char}
+                input = {input}
+                setInput = {setInput}
+                computeAnswer = {computeAnswer}
               />
             )
           })}
--- app/src/components/Btn.js
+++ app/src/components/Btn.js
@@ -1,13 +1,18 @@
 function Btn(props) {
-    const handleChar = (e) => {
-        console.log(e.target.innerText);
-    
+    const handleBtnInput = (id) => {
+        if (id < 15) {
+            props.setInput(props.input + props.char)
+        } else if (id === 15) { // clr btn
+            props.setInput('')
+        } else if (id === 16) { // enter btn
+            props.computeAnswer(props.input)
+        }
     }
 
     return (
         <button
             className={`char${props.id} char`}
-            onClick={handleChar} // add char to screen onClick
+            onClick={() => {handleBtnInput(props.id)}} // add char to screen onClick
         >
             {props.char}
         </button>
